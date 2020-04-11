171acc236db8c8bedd36c882b21b70274e9872fb
Minh Thu <MinhThuMinhThu0205@gmail.com>
2020-04-11 11:18:42 +0700

    mau velocity

---
+++ 1.js
@@ -0,0 +1,22 @@
+﻿ $(function(){
+ 	$(window).scroll(function(event) {
+ 		/* Act on the event */
+ 		if($('body,html').scrollTop()>=300){
+ 			$('.logo').addClass('chucam');
+ 			$('.top,.menuright,li,a').addClass('chuden');
+ 			$('.navbar').addClass('nentrang');
+ 			$('.nutcomback').addClass('hienra');
+ 		}
+ 		else if ($('body,html').scrollTop() < 100){
+ 			$('.logo').removeClass('chucam');
+ 			$('.top,.menuright,li,a').removeClass('chuden');
+ 			$('.navbar').removeClass('nentrang');
+ 			$('.nutcomback').removeClass('hienra');
+ 		}
+
+
+
+ 	});
+ 
+})  
+ 
